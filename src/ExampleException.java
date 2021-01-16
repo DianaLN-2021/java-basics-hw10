c0f0ee9b5ca185630ce78a9c79e95b25011f5ff3
Diana LN <dianejis@gmail.com>
2021-01-16 15:08:41 +0200

    AQA Homework 10 - Exceptions

--- src/ExampleException.java
+++ src/ExampleException.java
@@ -1,8 +1,12 @@
 public class ExampleException {
-  public static int rectangleArea(int a, int b) {
-    // TODO put your code here
-    return 0;
+
+  public static int rectangleArea(int a, int b) throws IllegalArgumentException {
+    int result;
+    if (a >= 0 && b >= 0) {
+        result = a * b;
+    } else {
+        throw new IllegalArgumentException("input value is below zero!");
+    }
+    return result;
   }
 }
-
-
